1e4d15222ebced8ad06ae015803309cc228e8984
Andy Rosa <andy.roshitu@gmail.com>
2022-11-12 21:14:24 +0300

    Добавляет отображение загружаемой фотографии

--- js/form.js
+++ js/form.js
@@ -1,6 +1,10 @@
 import { isEscapeKey } from './utils/utils.js';
 
+const FILE_TYPES = ['jpg', 'jpeg', 'png'];
+
 const form = document.querySelector('.img-upload__form');
+
+const miniPhotosEffects = document.querySelectorAll('.effects__preview');
 
 const formPhotoDownload = document.querySelector('.img-upload__overlay');
 const uploadFile = document.querySelector('#upload-file');
@@ -10,6 +14,17 @@ const hashtagField = document.querySelector('.text__hashtags');
 const descriptionField = document.querySelector('.text__description');
 
 const onFileInputChange = (evt) => openFormModal(evt);
+
+
+const preview = document.querySelector('.img-upload__preview img');
+
+const changePhotoPreview = (file) => {
+  preview.src = URL.createObjectURL(file);
+  miniPhotosEffects.forEach((photo) => {
+    photo.style.backgroundImage = `url(${URL.createObjectURL(file)})`;
+  });
+};
+
 
 const isTextFieldFocused = () =>
   document.activeElement === hashtagField ||
@@ -29,10 +44,17 @@ const onButtonCancelClick = () => {
 
 
 function openFormModal () {
-  formPhotoDownload.classList.remove('hidden');
-  document.body.classList.add('modal-open');
-  document.addEventListener('keydown', onPopupEscKeydown);
-  buttonCancel.addEventListener('click', onButtonCancelClick);
+  const file = uploadFile.files[0];
+  const fileName = file.name.toLowerCase();
+  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+
+  if (matches) {
+    formPhotoDownload.classList.remove('hidden');
+    document.body.classList.add('modal-open');
+    document.addEventListener('keydown', onPopupEscKeydown);
+    buttonCancel.addEventListener('click', onButtonCancelClick);
+    changePhotoPreview(file);
+  }
 }
 
 function closeFormModal () {
